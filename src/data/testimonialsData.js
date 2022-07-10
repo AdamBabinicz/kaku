b36f5cd408d87bd1825a3b2e33b178a32457ae80
babinicz <babinicz@onet.pl>
2022-07-10 14:41:20 +0200

    amiana

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -8,20 +8,20 @@ export const testimonialsData = [
     review:
       "Kiedy miałem osiem lat, wydarzyło się coś, co całkowicie zmieniło całe moje życie. Właśnie zmarł wielki naukowiec, a gazety opublikowały zdjęcie jego biurka z otwartą książką. A w podpisie było napisane, że jest to niedokończony rękopis największych naukowców naszych czasów. Chciał równania o długości nie większej niż jeden cal, które pozwoliłoby mu „czytać umysł Boga”. Więc powiedziałem sobie – wow, to jest dla mnie. To jest coś, co muszę zrobić.",
     name: "Jak odczytać myśli Boga?",
-    status: "www.chip.pl/2021/05/michio-kaku",
+    status: "www.chip.pl",
   },
   {
     image: image2,
     review:
       "Powiedzmy, że szachy są zasadami Wszechświata. Po 2000 lat, w końcu dowiedzieliśmy się, jak poruszają się pionki, jak porusza się goniec i rycerze. I przypuszczam, że pewnego dnia będziemy mieli równanie Boga, które powie nam, jak porusza się cała szachownica, a wtedy zostaniemy arcymistrzami. Będziemy w stanie zastosować to do odpowiedzi na niektóre z najgłębszych nierozwiązanych pytań we względności, na przykład, czy możliwe są podróże w czasie?",
     name: "Czy struny rządzą światem?",
-    status: "www.chip.pl/2021/05/michio-kaku",
+    status: "www.chip.pl",
   },
   {
     image: image3,
     review:
       "Nie jestem ateistą i nie sądzę, abym mógł się nazwać panteistą. Jesteśmy w położeniu małego dziecka wchodzącego do olbrzymiej biblioteki wypełnionej książkami w wielu różnych językach. Dziecko wie, że ktoś musiał te książki napisać. Nie wie ono jak. Nie rozumie języków, w których zostały one napisane. Dziecko mgliście podejrzewa, że ułożenie książek ma jakiś tajemniczy sens, ale go nie pojmuje.",
     name: "Cytat",
-    status: "lubimyczytac.pl/cytat/106581",
+    status: "lubimyczytac.pl",
   },
 ];
